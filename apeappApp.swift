2f73535f434c82c3fde020b3bf662366ad6b2602
Administrator <administrator@Administrators-MacBook-Pro-2.local>
2020-12-04 13:18:55 -0500

    initial commit for .swift files

---
+++ apeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  apeappApp.swift
+//  apeapp
+//
+//  Created by Administrator on 12/4/20.
+//
+
+import SwiftUI
+
+@main
+struct apeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
